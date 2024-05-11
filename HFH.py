e24199dffb55ae21956f46501e6ee3118dc08ecd
dhruv <dhruv20345@gmail.com>
2024-05-11 16:27:22 +0530

    agent prompt tutorials

--- HFH.py
+++ HFH.py
@@ -1,13 +1,17 @@
-MIXTRAL_TEXT_GENERATION_MODEL_REPO = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+# MIXTRAL_TEXT_GENERATION_MODEL_REPO = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 # TEXT_GENERATION_MODEL_REPO = "microsoft/Phi-3-mini-4k-instruct"
 # TEXT_GENERATION_MODEL_REPO = "meta-llama/Meta-Llama-3-8B"
-# TEXT_GENERATION_MODEL_REPO = "thenlper/gte-small"
+# TEXT_GENERATION_MODEL_REPO = "nvidia/Llama3-ChatQA-1.5-8B"
+TEXT_GENERATION_MODEL_REPO = "thenlper/gte-small"
 # TEXT_GENERATION_MODEL_REPO = "openai/whisper-large-v3"
 # TEXT_GENERATION_MODEL_REPO = "gradientai/Llama-3-8B-Instruct-Gradient-1048k"
 # TEXT_GENERATION_MODEL_REPO = "StabilityAI/stablelm-tuned-alpha-3b"
-# TEXT_GENERATION_MODEL_REPO = "Writer/camel-5b-hf"
-TEXT_GENERATION_MODEL_REPO = "HuggingFaceH4/zephyr-7b-alpha"
+# TEXT_GENERATION_MODEL_REPO = "HuggingFaceH4/zephyr-7b-alpha"
 # EMBEDDING_MODEL = "text-multilingual-embedding-preview-0409"
+# TEXT_GENERATION_MODEL_REPO = "microsoft/Phi-3-mini-4k-instruct"
+# TEXT_GENERATION_MODEL_REPO = "openai-community/gpt2"
+EMBEDDING_MODEL = "sentence-transformers/paraphrase-MiniLM-L6-v2"
+
 
 import logging
 import sys
@@ -23,7 +27,6 @@ print("DEVICE:", "GPU" if torch.cuda.is_available() else "CPU")
 
 
 from llama_index.core import Settings
-from transformers import AutoTokenizer
 from llama_index.embeddings.huggingface import (
     HuggingFaceInferenceAPIEmbedding,
     HuggingFaceEmbedding,
@@ -36,8 +39,7 @@ Settings.text_splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
 
 Settings.embed_model = HuggingFaceEmbedding(
     cache_folder="./tmp/models/",
-    # model_name=EMBEDDING_MODEL,
-    # tokenizer_name=EMBEDDING_MODEL,
+    model_name=EMBEDDING_MODEL,
 )
 
 #### Inference API
@@ -51,8 +53,10 @@ from llama_index.llms.gemini import Gemini
 Settings.llm = Gemini(temperature=0.82,max_tokens=10000)
 
 
-#### Local LLM
+# ### Local LLM
+# from transformers import pipeline
 
+# generator = pipeline("text-generation", model="gpt2")
 # Settings.llm = HuggingFaceLLM(
 #     model_name=TEXT_GENERATION_MODEL_REPO,
 #     tokenizer=AutoTokenizer.from_pretrained(
@@ -60,19 +64,23 @@ Settings.llm = Gemini(temperature=0.82,max_tokens=10000)
 #         padding_side="left",
 #     ),
 #     tokenizer_name=TEXT_GENERATION_MODEL_REPO,
-#     device_map="auto", # "cuda:0"
+#     device_map="cuda:0",  # "cuda:0"
 #     context_window=512,
-#     tokenizer_kwargs={"max_length": 2048},
-#     model_kwargs={"torch_dtype": torch.float16},
+#     tokenizer_kwargs={"max_length": 1024},
+#     model_kwargs={
+#         "torch_dtype": torch.float16,
+#         # "pad_token_id": generator.tokenizer.eos_token_id,
+#         "offload_folder": "./tmp/offload",
+#     },
 # )
 
 # Settings.llm = HuggingFaceLLM(
-#     context_window=2048,
-#     max_new_tokens=256,
+#     context_window=512,
+#     max_new_tokens=1024,
 #     generate_kwargs={"temperature": 0.25, "do_sample": False},
-#     tokenizer_name="Writer/camel-5b-hf",
-#     model_name="Writer/camel-5b-hf",
-#     device_map="auto",
+#     tokenizer_name=TEXT_GENERATION_MODEL_REPO,
+#     model_name=TEXT_GENERATION_MODEL_REPO,
+#     device_map="cuda",
 #     tokenizer_kwargs={"max_length": 2048},
 #     # uncomment this if using CUDA to reduce memory usage
 #     # model_kwargs={"torch_dtype": torch.float16}
